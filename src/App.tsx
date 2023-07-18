29dd4ef98338816187f52dbf974f817461f3bd0f
ruslan <zrm1306@gmail.com>
2023-07-18 12:08:12 +0500

    commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, officia.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs2.org"
           target="_blank"
           rel="noopener noreferrer"
         >
