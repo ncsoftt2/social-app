43fe9b68d73629b8f94ab640c904daa45561cac6
ruslan <zrm1306@gmail.com>
2023-07-18 12:10:47 +0500

    commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, officia.</p>
         <a
           className="App-link"
-          href="https://reactjs2.org"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
