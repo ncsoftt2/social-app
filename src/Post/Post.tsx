b2186640d63b73a992fdee554a2a3318e6f1da93
ruslan <zrm1306@gmail.com>
2023-07-19 11:25:40 +0500

    delete useless files, and start course

--- src/Post/Post.tsx
+++
@@ -1,5 +0,0 @@
-export const Post = () => {
-    return (
-        <div>Posts 3333</div>
-    )
-}
--- src/User/User.tsx
+++
@@ -1,5 +0,0 @@
-export const User = () => {
-    return (
-        <div>Users 44444</div>
-    )
-}
