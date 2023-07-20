b7721dd938b43bd4a48d57df081941c8057eac12
ruslan <zrm1306@gmail.com>
2023-07-20 13:33:43 +0500

    update dialogs and css for app

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,15 +1,70 @@
 import React from 'react';
+import styles from '../../styles/Dialogs.module.css'
+import {NavLink} from "react-router-dom";
+
+
+const image = 'https://cdn.theatlantic.com/media/img/photo/2018/10/images-of-the-season-fall-is-in-the/f02_RTX6EJJJ-1/original.jpg'
 
 const Dialogs = () => {
     return (
-        <section>
-            <ul>
-                <li>Jett</li>
-                <li>Reyna</li>
-                <li>Sage</li>
-                <li>Raze</li>
+        <section className={styles.section}>
+            <ul className={styles.dialogs_item}>
+                <NavLink to={'/dialogs/1'}
+                         className={({isActive}) => `${styles.link} ${isActive ? styles.active : null}` }
+                >
+                    <li className={styles.dialog}>
+                        <div>
+                            <img src={image} alt={"avatar"}/>
+                        </div>
+                        <div>Name 1</div>
+                    </li>
+                </NavLink>
+                <NavLink to={'/dialogs/2'}
+                         className={({isActive}) => `${styles.link} ${isActive ? styles.active : null}` }
+                >
+                    <li className={styles.dialog}>
+                        <div>
+                            <img src={image} alt={"avatar"}/>
+                        </div>
+                        <div>Name 2</div>
+                    </li>
+                </NavLink>
+                <NavLink to={'/dialogs/3'}
+                         className={({isActive}) => `${styles.link} ${isActive ? styles.active : null}` }
+                >
+                    <li className={styles.dialog}>
+                        <div>
+                            <img src={image} alt={"avatar"}/>
+                        </div>
+                        <div>Name 3</div>
+                    </li>
+                </NavLink>
+                {/*<li className={styles.dialog}>*/}
+                {/*    <NavLink to={'/dialogs/1'}*/}
+                {/*             className={({isActive}) => `${styles.link} ${isActive ? styles.active : null}` }*/}
+                {/*    >Jett</NavLink>*/}
+                {/*</li>*/}
+                {/*<li className={styles.dialog}>*/}
+                {/*    <NavLink to={'/dialogs/2'}*/}
+                {/*             className={({isActive}) => `${styles.link} ${isActive ? styles.active : null}` }*/}
+                {/*    >Sage</NavLink>*/}
+                {/*</li>*/}
+                {/*<li className={styles.dialog}>*/}
+                {/*    <NavLink to={'/dialogs/3'}*/}
+                {/*             className={({isActive}) => `${styles.link} ${isActive ? styles.active : null}` }*/}
+                {/*    >Reyna</NavLink>*/}
+                {/*</li>*/}
+                {/*<li className={styles.dialog}>*/}
+                {/*    <NavLink to={'/dialogs/4'}*/}
+                {/*             className={({isActive}) => `${styles.link} ${isActive ? styles.active : null}` }*/}
+                {/*    >Raze</NavLink>*/}
+                {/*</li>*/}
             </ul>
-            <div>messages</div>
+            <ul className={styles.messages_item}>
+                <li className={styles.message}>message 1</li>
+                <li className={styles.message}>useless message</li>
+                <li className={styles.message}>just text</li>
+            </ul>
         </section>
     );
 };
